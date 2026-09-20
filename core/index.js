85332f796bf97cb71089adc5f41751b8bf594b93
j67119172-code <j67119172@gmail.com>
2026-09-20 09:52:08 +0330

    Create index.js

---
+++ core/index.js
@@ -0,0 +1,8 @@
+// core/index.js
+export * from './constants.js';
+export * from './utils.js';
+export * from './rules.js';
+export * from './location.js';
+export * from './matching.js';
+export * from './validation.js';
+export * from './state.js';
