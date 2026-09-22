8bfb2ee92660ca22a2a32069f6b73efbffcc1339
j67119172-code <j67119172@gmail.com>
2026-09-22 13:12:35 +0330

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -12,6 +12,7 @@ import { errorHandler, notFoundHandler } from './middleware/errors.js';
 import { generalLimiter } from './middleware/rateLimit.js';
 import { authRouter } from './routes/auth.js';
 import { profileRouter } from './routes/profile.js';
+import { groupsRouter } from './routes/groups.js';
 
 const app = express();
 app.set('trust proxy', 1);
@@ -56,6 +57,7 @@ app.get('/health', async (_req, res) => {
 app.use(generalLimiter);
 app.use('/auth', authRouter);
 app.use('/me', profileRouter);
+app.use('/groups', groupsRouter);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
